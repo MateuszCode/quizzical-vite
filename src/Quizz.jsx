d954acc8d62b7e9cff0c24f39f1528a798fccb31
cybertecna <mjkopacki@gmail.com>
2024-01-17 20:38:42 +0100

    Removed unused lines of code

--- src/Quizz.jsx
+++ src/Quizz.jsx
@@ -46,7 +46,7 @@ export default function Quizz() {
     const questionElement = questionsData.map(questionInfo => {
         const {question, correctAnswer, id, allAnswers} = questionInfo 
         
-        const answersElement = allAnswers.shuffledAnswers.map((answer) => {
+        const answersElement = allAnswers.shuffledAnswers.map((answer,index) => {
                 let className = "answer-element"
 
                 for (let i = 0; i < chosenAnswers.length; i++) {
